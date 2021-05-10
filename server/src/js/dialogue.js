7c31de3f880acb0279bf30ffbcc6817394960df6
Karan Gandhi <karangandhi.programming@gmail.com>
2021-05-10 09:32:40 +0530

    completed the basic design of the dialogue box

--- server/src/js/dialogue.js
+++ server/src/js/dialogue.js
@@ -3,47 +3,56 @@ class PopupDialogueBox {
         this.title = title;
         this.children = [];
         this.domRoot = document.createElement("div");
-        this.domContent = document.createElement("div");
         this.submitCallback = submitCallback;
         this.createDialogueUI();
     }
 
     createDialogueUI() {
-        let box = document.createElement("div");
-        let head = document.createElement("div");
-        let title = document.createElement("span");
-        let footer = document.createElement("div");
-        let cancleButton = document.createElement("button");
-        let submitButton = document.createElement("button");
-
+        // let box = document.createElement("div");
+        // let head = document.createElement("div");
+        // let title = document.createElement("span");
+        // let footer = document.createElement("div");
+        // let cancleButton = document.createElement("button");
+        // let submitButton = document.createElement("button");
         this.domRoot.className = "dialogue-box-wrapper";
-        this.domContent.className = "dialogue-box-content";
-        box.className = "dialogue-box";
-        head.className = "dialogue-box-head";
-        title.className = "dialogue-box-title";
-        footer.className = "dialogue-box-footer";
-        cancleButton.className = "outline-button";
-        submitButton.className = "contained-button";
-
-        title.innerHTML = this.title;
-        cancleButton.innerHTML = "Cancle";
-        submitButton.innerHTML = "Submit";
-
-        head.appendChild(title);
-        footer.appendChild(submitButton);
-        footer.appendChild(cancleButton);
-        box.appendChild(head);
-        box.appendChild(this.domContent);
-        box.appendChild(footer);
-
-        // console.log(box);
-        setTimeout(() => this.domContent.appendChild(box), 10);
-
-        // console.log(this.domContent);
-        setTimeout(() => document.body.appendChild(this.domRoot), 100);
-
-        document.addEventListener("click", e => {
-            if (e.target.className.indexOf("dialogue") == -1) this.domRoot.remove();
-        });
+        this.domRoot.innerHTML = `<div class="dialogue-box">
+                                    <div class="dialogue-box-head">
+                                        <span class="dialogue-box-title">${this.title}</span>
+                                    </div>
+                                    <div class="dialogue-box-content"></div>
+                                    <div class="dialogue-box-footer">
+                                        <button class="outline-button" id="dialogue-cancle-button">Cancle</button>
+                                        <button class="contained-button" id="dialogue-submit-button">Submit</b  utton>
+                                    </div>
+                                </div>`;
+        // this.domContent.className = "dialogue-box-content";
+        // box.className = "dialogue-box";
+        // head.className = "dialogue-box-head";
+        // title.className = "dialogue-box-title";
+        // footer.className = "dialogue-box-footer";
+        // cancleButton.className = "outline-button";
+        // submitButton.className = "contained-button";
+        // title.innerHTML = this.title;
+        // cancleButton.innerHTML = "Cancle";
+        // submitButton.innerHTML = "Submit";
+        // head.appendChild(title);
+        // footer.appendChild(submitButton);
+        // footer.appendChild(cancleButton);
+        // box.appendChild(head);
+        // box.appendChild(this.domContent);
+        // box.appendChild(footer);
+        // // console.log(box);
+        // this.domContent.appendChild(box);
+        // // setTimeout(() => this.domContent.appendChild(box), 10);
+        // // console.log(this.domContent);
+        document.body.appendChild(this.domRoot);
+        // // setTimeout(() => document.body.appendChild(this.domRoot), 100);
+        setTimeout(
+            () =>
+                document.addEventListener("click", e => {
+                    if (e.target.className.indexOf("dialogue") == -1 || e.target.className === "dialogue-box-wrapper") this.domRoot.remove();
+                }),
+            10
+        );
     }
 }
