1b631c79d70f996c715c6912831d14891b851145
Karan Gandhi <karangandhi.programming@gmail.com>
2021-10-02 10:35:12 +0530

    setup firebase

---
+++ server/services/firebase.ts
@@ -0,0 +1,8 @@
+import * as admin from "firebase-admin";
+import serviceAccount from "../credentials/serviceAccount.json";
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+});
+
+export const db = admin.firestore();
---
+++ server/services/firestore.ts
@@ -0,0 +1,5 @@
+export const addData = async () => {};
+
+export const readData = async () => {};
+
+export const deleteData = async () => {};
