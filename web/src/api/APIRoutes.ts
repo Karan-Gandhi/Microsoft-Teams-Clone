d12fef6a59643e3320a00b3bbafb179fcfedae9f
Karan Gandhi <karangandhi.programming@gmail.com>
2021-10-03 15:16:04 +0530

    added the routes

---
+++ web/src/api/APIRoutes.ts
@@ -0,0 +1,9 @@
+enum APIRoutes {
+	LOGIN = "/auth/loginWithEmailAndPassword",
+	CREATE_USER = "/auth/createUserWithEmailAndPassword",
+	RENEW_ACCESS_TOKEN = "/auth/accessToken",
+	VERIFY_ACCESS_TOKEN = "/api",
+	LOGOUT = "/auth/logout",
+}
+
+export default APIRoutes;
